9ec77aee59456b83d16e7a102abfda8f41af2cba
chenyuxuan2009 <chen20091119@foxmail.com>
2025-03-27 21:56:38 +0800

    修复了一些小 bug

--- luogu_submission_better.js
+++ luogu_submission_better.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         洛谷提交记录显示优化
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @description  修改提交记录背景
 // @author       沉石鱼惊旋
 // @match        *://www.luogu.com.cn/record/*
@@ -27,24 +27,24 @@ function main() {
     let len = tc.length;
     for (let i = 0; i < len; i += 1) {
         if (tc[i].getElementsByClassName("spinner").length) {
-            tc[i].style = `background: linear-gradient(rgba(20, 85, 143, 0.3), rgba(20, 85, 143, 0.3)), url('${Judging}'); background - size: cover; `;
+            tc[i].style = `background: linear-gradient(rgba(20, 85, 143, 0.3), rgba(20, 85, 143, 0.3)), url('${Judging}'); background-size: cover;`;
             continue;
         }
         let status = tc[i].getElementsByClassName('status')[0].innerHTML.substring(0, 2);
         if (status === "AC") {
-            tc[i].style = `background: linear - gradient(rgba(82, 196, 26, 0.3), rgba(82, 196, 26, 0.3)), url('${AC}'); background - size: cover; `;
+            tc[i].style = `background: linear-gradient(rgba(82, 196, 26, 0.3), rgba(82, 196, 26, 0.3)), url('${AC}'); background-size: cover;`;
         } else if (status === "WA") {
-            tc[i].style = `background: linear - gradient(rgba(231, 76, 60, 0.3), rgba(231, 76, 60, 0.3)), url('${WA}'); background - size: cover; `;
+            tc[i].style = `background: linear-gradient(rgba(231, 76, 60, 0.3), rgba(231, 76, 60, 0.3)), url('${WA}'); background-size: cover;`;
         } else if (status === "TL") {
-            tc[i].style = `background: linear - gradient(rgba(5, 34, 66, 0.3), rgba(5, 34, 66, 0.3)), url('${TLE}'); background - size: cover; `;
+            tc[i].style = `background: linear-gradient(rgba(5, 34, 66, 0.3), rgba(5, 34, 66, 0.3)), url('${TLE}'); background-size: cover;`;
         } else if (status === "ML") {
-            tc[i].style = `background: linear - gradient(rgba(5, 34, 66, 0.3), rgba(5, 34, 66, 0.3)), url('${MLE}'); background - size: cover; `;
+            tc[i].style = `background: linear-gradient(rgba(5, 34, 66, 0.3), rgba(5, 34, 66, 0.3)), url('${MLE}'); background-size: cover;`;
         } else if (status === "RE") {
-            tc[i].style = `background: linear - gradient(rgba(157, 61, 207, 0.3), rgba(157, 61, 207, 0.3)), url('${RE}'); background - size: cover; `;
+            tc[i].style = `background: linear-gradient(rgba(157, 61, 207, 0.3), rgba(157, 61, 207, 0.3)), url('${RE}'); background-size: cover;`;
         } else if (status === "OL") {
-            tc[i].style = `background: linear - gradient(rgba(5, 34, 66, 0.3), rgba(5, 34, 66, 0.3)), url('${OLE}'); background - size: cover; `;
+            tc[i].style = `background: linear-gradient(rgba(5, 34, 66, 0.3), rgba(5, 34, 66, 0.3)), url('${OLE}'); background-size: cover;`;
         } else if (status === "UK") {
-            tc[i].style = `background: linear - gradient(rgba(14, 29, 105, 0.3), rgba(14, 29, 105, 0.3)), url('${UKE}'); background - size: cover; `;
+            tc[i].style = `background: linear-gradient(rgba(14, 29, 105, 0.3), rgba(14, 29, 105, 0.3)), url('${UKE}'); background-size: cover;`;
         }
     }
 };
